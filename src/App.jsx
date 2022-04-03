408c7271de0ec774d902793d98c6c93dac5504e6
Shaxzod <mastermusic2529@gmail.com>
2022-04-03 16:28:27 +0500

    initialize commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import './App.scss';
 
-import { Switch, Route } from 'react-router';
-
 import Header from './components/Header/Header';
-
-// ICONS
-
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Switch>
-        <Route path="/" component={Header}></Route>
-      </Switch> */}
+      sds
     </div>
   );
 }
